7aa87dd3b729350bf1cf6c25cba9feeb289c3c55
jmayta1984 <jmayta@pucp.edu.pe>
2025-06-10 18:16:08 -0500

    feat (Home): update ShoeDetailPage

--- demo/lib/features/home/presentation/pages/shoe_detail_page.dart
+++ demo/lib/features/home/presentation/pages/shoe_detail_page.dart
@@ -28,118 +28,116 @@ class _ShoeDetailPageState extends State<ShoeDetailPage> {
           child: Text("Add to cart"),
         ),
       ),
-      body: SafeArea(
-        child: NestedScrollView(
-          headerSliverBuilder: (context, innerBoxIsScrolled) {
-            return [
-              SliverAppBar(
-                expandedHeight: 300,
-                pinned: true,
-                floating: false,
-                flexibleSpace: FlexibleSpaceBar(
-                  background: Stack(
-                    children: [
-                      Center(
-                        child: Hero(
-                          tag: widget.shoe.id,
-                          child: SizedBox(
-                            height: 200,
-                            width: double.infinity,
-                            child: Image.network(
-                              widget.shoe.image,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        top: 60,
-                        right: 0,
-                        child: IconButton(
-                          onPressed: () {},
-                          icon: Icon(Icons.favorite_border),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ];
-          },
-          body: Column(
-            spacing: 16,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text(
-                widget.shoe.name,
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-              ),
-              Text(widget.shoe.description),
-
-              Text("Size", style: TextStyle(fontWeight: FontWeight.bold)),
-
-              SizedBox(
-                height: 40,
-                child: ListView.separated(
-                  separatorBuilder: (context, index) => SizedBox(width: 8),
-                  scrollDirection: Axis.horizontal,
-                  itemCount: sizes.length,
-                  itemBuilder: (context, index) {
-                    final size = sizes[index];
-                    final isSelected = index == _selectedSizeIndex;
-                    return Center(
-                      child: GestureDetector(
-                        onTap: () => setState(() {
-                          _selectedSizeIndex = index;
-                        }),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                              color: isSelected
-                                  ? ColorPalette.primaryColor
-                                  : ColorPalette.background,
-                            ),
-                            borderRadius: BorderRadius.circular(8),
-
-                            color: isSelected
-                                ? ColorPalette.primaryColor
-                                : ColorPalette.white,
-                          ),
-                          width: 40,
-                          height: 40,
-                          child: Center(
-                            child: Text(
-                              size.size,
-                              style: TextStyle(
-                                color: isSelected
-                                    ? ColorPalette.white
-                                    : ColorPalette.background,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-              if (_selectedSizeIndex >= 0)
-                Row(
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) {
+          return [
+            SliverAppBar(
+              expandedHeight: 300,
+              pinned: true,
+              floating: false,
+              flexibleSpace: FlexibleSpaceBar(
+                background: Stack(
                   children: [
-                    SizedBox(
-                      width: 48,
-                      height: 24,
-                      child: Center(
-                        child: Text(
-                          sizes[_selectedSizeIndex].quantity.toString(),
+                    Center(
+                      child: Hero(
+                        tag: widget.shoe.id,
+                        child: SizedBox(
+                          height: 200,
+                          width: double.infinity,
+                          child: Image.network(
+                            widget.shoe.image,
+                            fit: BoxFit.cover,
+                          ),
                         ),
                       ),
                     ),
-                    Spacer(),
+                    Positioned(
+                      top: 60,
+                      right: 0,
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.favorite_border),
+                      ),
+                    ),
                   ],
                 ),
-            ],
-          ),
+              ),
+            ),
+          ];
+        },
+        body: Column(
+          spacing: 16,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              widget.shoe.name,
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+            ),
+            Text(widget.shoe.description),
+      
+            Text("Size", style: TextStyle(fontWeight: FontWeight.bold)),
+      
+            SizedBox(
+              height: 40,
+              child: ListView.separated(
+                separatorBuilder: (context, index) => SizedBox(width: 8),
+                scrollDirection: Axis.horizontal,
+                itemCount: sizes.length,
+                itemBuilder: (context, index) {
+                  final size = sizes[index];
+                  final isSelected = index == _selectedSizeIndex;
+                  return Center(
+                    child: GestureDetector(
+                      onTap: () => setState(() {
+                        _selectedSizeIndex = index;
+                      }),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: isSelected
+                                ? ColorPalette.primaryColor
+                                : ColorPalette.background,
+                          ),
+                          borderRadius: BorderRadius.circular(8),
+      
+                          color: isSelected
+                              ? ColorPalette.primaryColor
+                              : ColorPalette.white,
+                        ),
+                        width: 40,
+                        height: 40,
+                        child: Center(
+                          child: Text(
+                            size.size,
+                            style: TextStyle(
+                              color: isSelected
+                                  ? ColorPalette.white
+                                  : ColorPalette.background,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            if (_selectedSizeIndex >= 0)
+              Row(
+                children: [
+                  SizedBox(
+                    width: 48,
+                    height: 24,
+                    child: Center(
+                      child: Text(
+                        sizes[_selectedSizeIndex].quantity.toString(),
+                      ),
+                    ),
+                  ),
+                  Spacer(),
+                ],
+              ),
+          ],
         ),
       ),
     );
