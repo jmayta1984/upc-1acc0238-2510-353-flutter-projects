a9934ce495da6aa3ead778017c1d9151eae8a267
jmayta1984 <jmayta@pucp.edu.pe>
2025-06-10 18:14:18 -0500

    feat (Auth): update LoginPage

--- demo/lib/features/auth/presentation/pages/login_page.dart
+++ demo/lib/features/auth/presentation/pages/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:demo/core/themes/color_palette.dart';
 import 'package:demo/features/app/presentation/pages/main_view.dart';
 import 'package:demo/features/auth/presentation/blocs/auth_bloc.dart';
 import 'package:demo/features/auth/presentation/blocs/auth_event.dart';
@@ -15,8 +16,8 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   bool _isVisible = false;
-  TextEditingController _usernameController = TextEditingController();
-  TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -38,94 +39,119 @@ class _LoginPageState extends State<LoginPage> {
           ).showSnackBar(SnackBar(content: Text(state.errorMessage)));
         }
       },
-      child: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                controller: _usernameController,
-                cursorColor: Color.fromRGBO(255, 107, 53, 1),
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.person),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      width: 2,
-                      color: Color.fromRGBO(255, 107, 53, 1),
-                    ),
-                    borderRadius: BorderRadius.circular(24),
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(24),
-                  ),
-                  hintText: "Username",
+      child: BlocBuilder<AuthBloc, AuthState>(
+        builder: (context, state) {
+          final isLoading = (state is LoadingAuthState);
+          return Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: isLoading ? Colors.grey.shade100 : Colors.transparent,
                 ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                controller: _passwordController,
-                cursorColor: Color.fromRGBO(255, 107, 53, 1),
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.lock),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      width: 2,
-                      color: Color.fromRGBO(255, 107, 53, 1),
-                    ),
-                    borderRadius: BorderRadius.circular(24),
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(24),
-                  ),
-                  suffixIcon: IconButton(
-                    onPressed: () => {
-                      setState(() {
-                        _isVisible = !_isVisible;
-                      }),
-                    },
-                    icon: Icon(
-                      _isVisible ? Icons.visibility : Icons.visibility_off,
-                    ),
-                  ),
-                  hintText: "Password",
-                ),
-                obscureText: !_isVisible,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SizedBox(
-                height: 48,
-                width: double.infinity,
-                child: OutlinedButton(
-                  style: OutlinedButton.styleFrom(
-                    side: BorderSide(
-                      width: 1.0,
-                      color: Color.fromRGBO(255, 107, 53, 1),
-                    ),
-                    foregroundColor: Colors.white,
-                    backgroundColor: Color.fromRGBO(255, 107, 53, 1),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadiusGeometry.circular(32),
-                    ),
-                  ),
-                  onPressed: () => {
-                    context.read<AuthBloc>().add(
-                      LoginEvent(
-                        username: _usernameController.text,
-                        password: _passwordController.text,
+                child: AbsorbPointer(
+                  absorbing: isLoading,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextField(
+                          autocorrect: false,
+                          controller: _usernameController,
+                          cursorColor: ColorPalette.primaryColor,
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.person),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                width: 2,
+                                color: ColorPalette.primaryColor,
+                              ),
+                              borderRadius: BorderRadius.circular(24),
+                            ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(24),
+                            ),
+                            hintText: "Username",
+                          ),
+                        ),
                       ),
-                    ),
-                  },
-                  child: Text("Sign in"),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextField(
+                          autocorrect: false,
+                          controller: _passwordController,
+                          cursorColor: ColorPalette.primaryColor,
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.lock),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                width: 2,
+                                color: ColorPalette.primaryColor,
+                              ),
+                              borderRadius: BorderRadius.circular(24),
+                            ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(24),
+                            ),
+                            suffixIcon: IconButton(
+                              onPressed: () => {
+                                setState(() {
+                                  _isVisible = !_isVisible;
+                                }),
+                              },
+                              icon: Icon(
+                                _isVisible
+                                    ? Icons.visibility
+                                    : Icons.visibility_off,
+                              ),
+                            ),
+                            hintText: "Password",
+                          ),
+                          obscureText: !_isVisible,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: SizedBox(
+                          height: 48,
+                          width: double.infinity,
+                          child: OutlinedButton(
+                            style: OutlinedButton.styleFrom(
+                              side: BorderSide(
+                                width: 1.0,
+                                color: ColorPalette.primaryColor,
+                              ),
+                              foregroundColor: Colors.white,
+                              backgroundColor: ColorPalette.primaryColor,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadiusGeometry.circular(32),
+                              ),
+                            ),
+                            onPressed: () => {
+                              context.read<AuthBloc>().add(
+                                LoginEvent(
+                                  username: _usernameController.text,
+                                  password: _passwordController.text,
+                                ),
+                              ),
+                            },
+                            child: Text("Sign in"),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
-        ),
+              if (isLoading)
+                Center(
+                  child: CircularProgressIndicator(
+                    color: ColorPalette.primaryColor,
+                  ),
+                ),
+            ],
+          );
+        },
       ),
     );
   }
